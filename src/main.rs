5670357d8b072a7a716f26655af73ef89ea99d50
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-03-24 01:10:00 +0100

    Updates

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,96 @@
-fn handle_client_connection(mut stream: TcpStream) {
-    // Demande le mot de passe
-    let mut data = [0 as u8; 1024];
-    stream.write(b"Entrez le mot de passe: ").unwrap();
-    let size = stream.read(&mut data).unwrap();
-    let password = std::str::from_utf8(&data[0..size]).unwrap().trim();
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
+use std::thread;
 
-    // Vérifie le mot de passe
-    if password != "my_secret_password" {
-        stream.write(b"Mot de passe incorrect").unwrap();
+fn handle_client(stream: TcpStream, users: Arc<Mutex<Vec<String>>>) {
+    let mut stream = BufReader::new(stream);
+    let mut line = String::new();
+
+   
+    let password = "my_secret_password";
+    write!(stream.get_mut(), "Enter password: ").unwrap();
+    stream.get_mut().flush().unwrap();
+
+   
+    stream.read_line(&mut line).unwrap();
+    let password_attempt = line.trim();
+
+    
+    if password_attempt != password {
+        write!(stream.get_mut(), "Incorrect password\n").unwrap();
         return;
     }
 
-    // Demande le nom d'utilisateur
-    stream.write(b"Entrez votre nom d'utilisateur: ").unwrap();
-    let size = stream.read(&mut data).unwrap();
-    let username = std::str::from_utf8(&data[0..size]).unwrap().trim();
+   
+    line.clear();
+    write!(stream.get_mut(), "Enter username: ").unwrap();
+    stream.get_mut().flush().unwrap();
 
-    stream.write(b"Connexion réussie").unwrap();
+   
+    stream.read_line(&mut line).unwrap();
+    let username = line.trim().to_owned();
 
-    // Ajoute l'utilisateur à la liste
-    let mut users = USERS.lock().unwrap();
-    users.insert(stream.try_clone().unwrap(), String::from(username));
+    
+    users.lock().unwrap().push(username.clone());
 
-    // Envoie le message de bienvenue
-    let message = format!("{} a rejoint le chat\n", username);
-    broadcast_message(&stream, &message);
+   
+    let message = format!("{} has joined the chat\n", username);
+    println!("{}", message);
+    for user in users.lock().unwrap().iter() {
+        let mut user_stream = stream.get_ref().try_clone().unwrap();
+        write!(user_stream, "{}", message).unwrap();
+        user_stream.flush().unwrap();
+    }
 
-    // Lit les messages entrants et les diffuse à tous les utilisateurs
+   
     loop {
-        let mut data = [0 as u8; 1024];
-        let size = stream.read(&mut data).unwrap();
-        let message = std::str::from_utf8(&data[0..size]).unwrap().trim();
+        line.clear();
+        stream.read_line(&mut line).unwrap();
+        let message = line.trim().to_owned();
 
-        if message == "/quit" {
+      
+        if message.is_empty() {
+            let index = users.lock().unwrap().iter().position(|u| u == &username).unwrap();
+            users.lock().unwrap().remove(index);
+            let message = format!("{} has left the chat\n", username);
+            println!("{}", message);
+            for user in users.lock().unwrap().iter() {
+                let mut user_stream = stream.get_ref().try_clone().unwrap();
+                write!(user_stream, "{}", message).unwrap();
+                user_stream.flush().unwrap();
+            }
             break;
         }
 
+      
         let message = format!("{}: {}\n", username, message);
-        broadcast_message(&stream, &message);
+        println!("{}", message);
+        for user in users.lock().unwrap().iter() {
+            if user != &username {
+                let mut user_stream = stream.get_ref().try_clone().unwrap();
+                write!(user_stream, "{}", message).unwrap();
+                user_stream.flush().unwrap();
+            }
+        }
     }
+}
 
-    // Retire l'utilisateur de la liste et envoie un message de départ
-    users.remove(&stream.try_clone().unwrap());
-    let message = format!("{} a quitté le chat\n", username);
-    broadcast_message(&stream, &message);
+fn main() {
+    let listener = TcpListener::bind("localhost:5000").unwrap();
+    let users = Arc::new(Mutex::new(Vec::new()));
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let users = users.clone();
+                thread::spawn(move || {
+                    handle_client(stream, users);
+                });
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+            }
+        }
+    }
 }
