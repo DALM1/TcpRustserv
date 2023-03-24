1fe51c65975dac3b059ea9189bf6e93a1995981d
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-03-24 01:18:51 +0100

    Updates

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,85 @@
-use std::collections::HashMap;
-use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
 use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
 use std::thread;
 
-fn handle_client(mut stream: TcpStream, password: &str, users: Arc<Mutex<HashMap<TcpStream, String>>>) {
+type UserList = Arc<Mutex<HashMap<TcpStream, String>>>;
+
+fn handle_client(mut stream: TcpStream, password: &str, users: &UserList) {
     let mut data = [0 as u8; 1024];
     
-   
+    // Demande du mot de passe
     stream.write(b"Entrez le mot de passe: ").unwrap();
     let size = stream.read(&mut data).unwrap();
     let password_input = std::str::from_utf8(&data[0..size]).unwrap().trim();
 
-    
+    // Vérification du mot de passe
     if password_input != password {
         stream.write(b"Mot de passe incorrect").unwrap();
         return;
     }
 
-    // Demander le nom d'utilisateur
+    // Demande du nom d'utilisateur
     stream.write(b"Entrez votre nom d'utilisateur: ").unwrap();
     let size = stream.read(&mut data).unwrap();
     let username = std::str::from_utf8(&data[0..size]).unwrap().trim();
 
-   
-    stream.write(b"Connexion réussie").unwrap();
+    // Connexion réussie
+    stream.write(b"Connexion réussie\n").unwrap();
 
-    
+    // Ajout de l'utilisateur à la liste des utilisateurs connectés
     let mut users = users.lock().unwrap();
     users.insert(stream.try_clone().unwrap(), String::from(username));
 
-   
+    // Diffusion d'un message informant que l'utilisateur s'est connecté
     let message = format!("{} a rejoint le chat\n", username);
     broadcast_message(&stream, &message, &users);
 
-   
+    // Boucle de lecture des messages envoyés par l'utilisateur
     loop {
         let mut data = [0 as u8; 1024];
         let size = stream.read(&mut data).unwrap();
         let message = std::str::from_utf8(&data[0..size]).unwrap().trim();
 
-        // Quitter le chat si l'utilisateur envoie la commande /quit
+        // Arrêt du traitement si l'utilisateur a saisi "/quit"
         if message == "/quit" {
             break;
         }
 
-        // Diffuser le message de l'utilisateur
+        // Diffusion du message de l'utilisateur aux autres utilisateurs
         let message = format!("{}: {}\n", username, message);
         broadcast_message(&stream, &message, &users);
     }
 
-    // Retirer l'utilisateur de la liste
+    // Retrait de l'utilisateur de la liste des utilisateurs connectés
     users.remove(&stream.try_clone().unwrap());
 
-    // Diffuser le message de déconnexion
+    // Diffusion d'un message informant que l'utilisateur s'est déconnecté
     let message = format!("{} a quitté le chat\n", username);
     broadcast_message(&stream, &message, &users);
 }
 
-fn broadcast_message(sender: &TcpStream, message: &str, users: &Arc<Mutex<HashMap<TcpStream, String>>>) {
-    let users = users.lock().unwrap();
-    for client in users.keys() {
+fn broadcast_message(sender: &TcpStream, message: &str, users: &UserList) {
+    let mut to_remove = Vec::new();
+
+    for (client, _) in users.lock().unwrap().iter() {
         if client.peer_addr().unwrap() != sender.peer_addr().unwrap() {
-            client.write(message.as_bytes()).unwrap();
+            if let Err(_) = client.write(message.as_bytes()) {
+                to_remove.push(client.clone());
+            }
         }
+    }
+
+    let mut users = users.lock().unwrap();
+    for client in to_remove {
+        users.remove(&client);
     }
 }
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:5000")?;
-    let password = "my_secret_password".to_string();
+    let password = "my_secret_password".to_string(); // Définition du mot de passe
     let users = Arc::new(Mutex::new(HashMap::new()));
-    println!("Serveur en attente de connexions sur le port 5000...");
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                let password = password.clone();
-                let users = users.clone();
-                thread::spawn(move || {
-                    handle_client(stream, &password, users);
-                });
-            }
-            Err(e) =>
+    println!("Serveur en attente de connexions sur le
