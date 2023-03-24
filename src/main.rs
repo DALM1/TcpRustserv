814cf09876d448a27a825e96545026149a528a75
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-03-24 01:33:04 +0100

    Updates

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use std::net::{TcpListener, TcpStream};
 use std::thread;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    let password = "password";
+    let listener = TcpListener::bind("127.0.0.1:5000").unwrap();
+    let password = "my_scret_password";
     let clients: HashMap<String, TcpStream> = HashMap::new();
 
     for stream in listener.incoming() {
